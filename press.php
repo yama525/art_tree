e26f01e1c130d349cd20ffb083823aa70b032c02
yamajiji <myy.yama1008@gmail.com>
2021-12-25 21:00:48 +0900

    css をリンクしたのと、reference フォルダに「俺のギャラリー」コード入れました

--- press.php
+++ press.php
@@ -1,2 +1,16 @@
 <!-- お知らせ・案内ページ -->
 <!-- このページは案内を押すと別のサイトにリンクします -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="scss/main.css">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
