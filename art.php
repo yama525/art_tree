62f0ae11c357d6e392f3ab1b2dee9175dd5e9fe1
junji.n <june4jii@gmail.com>
2022-01-06 15:51:55 +0900

    アート一覧表示からart_detail.phpへの遷移リンク追加

--- art.php
+++ art.php
@@ -36,7 +36,7 @@ $status_join_follow_art = $stmt_join_follow_art->execute();
 $view_join_follow_art = "";
 while($result_join_follow_art = $stmt_join_follow_art->fetch(PDO::FETCH_ASSOC)){
   // var_dump($result_join_follow_art["u_img"]);
-  $view_join_follow_art .= '<li><img src="art_img/'.$result_join_follow_art["a_img"].'"></li>';
+  $view_join_follow_art .= '<li><a href="art_detail.php"><img src="art_img/'.$result_join_follow_art["a_img"].'"></a></li>';
 }
 
 
@@ -53,6 +53,7 @@ while($result_join_follow_art = $stmt_join_follow_art->fetch(PDO::FETCH_ASSOC)){
     <title>Document</title>
 </head>
 <body>
+
 
 <!-- ------------------------------------------------------ -->
 <!---------------------- ここから header ---------------------->
